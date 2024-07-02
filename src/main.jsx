e4c9d73d49d1620edc923d9a89025f1baf9a5502
Karan Singh <122381186+Royalkarang@users.noreply.github.com>
2024-07-02 12:18:33 +0530

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <div className='flex items-center justify-center h-[100vh] bg-slate-950 text-red-600 text-5xl'> 
+      Hello World !
+    </div>
+  </React.StrictMode>,
+)
